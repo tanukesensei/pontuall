694dd278e01aa3c2124882d17602ad64b12b7bdc
Luan <luanvs@hotmail.com>
2015-10-01 23:10:16 -0300

    primeiro commit no notebook

--- class/Usuario.class.php
+++ class/Usuario.class.php
@@ -1,57 +1,110 @@
-<?php 
-include_once 'BD.class.php';
+<?php
 
+include_once 'BD.class.php';
 
 class Usuario
 {	
 	private $nome;
 	private $sobrenome;
 	private $cpf;
-	public $email;
-	public $senha;
-	public $username;
-	protected $BD;
+	private $email;
+	private $senha;
+	private $username;
+	private $bd;
+	private $tabela;
+	private $id;
 
-	function __construct(){
-		$this->BD = new BD();
-	}
+	public function __construct() {
+        $this->bd = new BD();
+        $this->tabela = "users";
+    }
+    public function __destruct() {
+        unset($this->bd);
+    }
 
+    public function __get($key) {
+        return $this->$key;
+    }
 
-	 public function cadastro($nome, $sobrenome, $cpf, $email, $username, $senha)
-	{	
-		$this->nome 	 = $nome;
-		$this->sobrenome = $sobrenome;
-		$this->cpf 		 = $cpf;
-		$this->email 	 = $email;
-		$this->senha 	 = $senha;
-		$this->username	 = $username;
+    //método de retorno de valores do objeto 
+    public function __set($key, $value) {
+        $this->$key = $value;
+    }
 
-		$result = $BD->pg_query("INSERT INTO users (name_exp, last_name, cpf, email, password, username)
-			VALUES ('$nome, $sobrenome, $cpf, $email, $senha, $username')");
-		if (isset($result)) {
-			echo "Usuario cadastrado com sucesso.";
-		} else {
-			echo "erro ao cadastrar.";
-		}
-	}
+    //METODOS 
+    //BANCO DE DADOS
+    public function inserir() {
+        
+        $sql = "INSERT INTO $this->tabela (name_exp, last_name, cpf, email, password, username) values 
+        ('$this->nome','$this->sobrenome','$this->cpf', '$this->email','$this->senha', '$this->username')";
+        
+        echo $sql;
+        $retorno = pg_query($sql);
+        return $retorno;
+    }
 
-	public function login($login, $senha)
-	{
-		$this->login = $login;
-		$this->senha = $senha;
+    public function listar($complemento = "") {
+        $sql = "SELECT * FROM $this->tabela ".
+                $complemento;
 
-		$verify = $DB->query("SELECT name_exp, password FROM users");
+        $resultado = pg_query($sql);
+        $retorno = NULL;
+        //percorre os registros
+        while ($reg = pg_fetch_assoc($resultado)) {
+            //transforma em objetos categoria
+            $obj = new Usuario();
+            $obj->id = $reg["id"];
+            $obj->nome = $reg["nome"];
+            $obj->sobrenome = $reg["sobrenome"];
+            $obj->cpf = $reg["cpf"];
+            $obj->email = $reg["email"];
+            $obj->senha = $reg["senha"];
+            $obj->username = $reg["username"];
+            //adiciona a variavel de retorno
+            $retorno[] = $obj;
+        }
+        return $retorno;
+    }
+	
+    public function excluir() {
+        
+        $sql = "delete from $this->tabela where id = $this->id";
+        $retorno = pg_query($sql);
+        return $retorno;
+    }
 
-		if ($verify) {
-			foreach ($verify as $row) {
-				$verify['name_exp'] == $login;
-				$verify['password'] == $senha;
-			}
-		} else {
-			echo "Login ou senha inválidos, por favor tente novamente mais tarde";
-		}
-	}
+    public function atualizar() {
+        $retorno = false;
+        $sql = "update $this->tabela set name_exp ='$this->nome', last_name ='$this->sobrenome', cpf ='$this->cpf', email ='$this->email', password ='$this->senha', username ='$this->username' where
+                       id = $this->id";
+        $retorno = pg_query($sql);
+        return $retorno;
+    }
+
+    public function retornarunico() {
+        $sql = "Select * FROM $this->tabela where id=$this->id";
+
+        $resultado = pg_query($sql);
+        $retorno = NULL;
+
+        $req = pg_fetch_assoc($resultado);
+        if ($req == true) {
+            $obj = new Usuario();
+            $obj->id = $reg["id"];
+            $obj->nome = $reg["nome"];
+            $obj->sobrenome = $reg["sobrenome"];
+            $obj->cpf = $reg["cpf"];
+            $obj->email = $reg["email"];
+            $obj->senha = $reg["senha"];
+            $obj->username = $reg["username"];
+            //adiciona a variavel de retorno
+            $retorno[] = $obj;
+        } else {
+            $retorno = null;
+        }
+
+        return $retorno;
+    }
 
 }
-
- ?>
+?>
