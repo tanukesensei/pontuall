f407db55ee75003ee81d8cc2b02b46091c6a449e
Luan <luanvs@hotmail.com>
2016-07-26 16:37:08 -0300

    + modificações

--- admin/top_menu.php
+++ admin/top_menu.php
@@ -1,5 +1,3 @@
-
-
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -13,12 +11,12 @@
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
-        
+
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['username']; ?> <b class="caret"></b></a>
             <ul class="dropdown-menu">
                 <li>
-                    <a href="process_new.php"><i class="fa fa-fw fa-user"></i> Perfil</a>
+                    <a href="#"><i class="fa fa-fw fa-user"></i> Perfil</a>
                 </li>
                 <li>
                     <a href="process_new.php"><i class="fa fa-fw fa-envelope"></i> Novo Processo</a>
@@ -34,14 +32,3 @@
         </li>
     </ul>
 </nav>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
--- php/user_process.php
+++ php/user_process.php
@@ -14,79 +14,54 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Pontuall</title>
-
-
-	 <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="../css/bootstrap.css" type="text/css">
-
-    <!-- Custom Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css" type="text/css">
-
-    <!-- Plugin CSS -->
-    <link rel="stylesheet" href="../css/animate.min.css" type="text/css">
-
+    <!-- Bootstrap Core CSS -->
+    <link href="../admin/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="../css/creative.css" type="text/css">
+    <link href="../admin/css/sb-admin.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="../admin/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
 </head>
 
 
 
-<body id="calc">
-     <?php include "../admin/top_menu.php"; ?>
-	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Pontuall</a>
-            </div>
+<body>
+     <div id="wrapper">
+       <?php include "../admin/top_menu.php"; ?>
+       <div id="page-wrapper">
+         <div class="container-fluid">
+          <div class="header-content">
+              <div class="header-content-inner">
+                <div class="exibe_processo">
+                <?php
+                    $objProcesso = new Processo();
+                    $complemento = "ORDER BY id DESC";
+                    $lista = $objProcesso->listar($complemento);
+                    $linhas = "";
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a class="novo" href="process_new.php"> Novo Processo </a>
-                    </li>
-                    <li>
-                        <a class="editar" href="user_edit.php">Usuário: <?php echo $_SESSION['username']; ?> </a>
-                    </li>
-                    <li>
-                        <a class="sair" href="logout.php">Sair</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+                    if ($lista != null) {
+                        foreach ($lista as $processo) {
+                          $exibeProcesso = $linhas."<strong><a href='cartao_ponto.php?id=".$processo->id."'>".$processo->nome_reclamante." Contra ".$processo->nome_reclamado.", ".$processo->num_processo."</strong><br />";
+                          echo $exibeProcesso ;
+                        }
+                    }
+                 ?>
+              </div>
+          </div>
         </div>
-        <!-- /.container-fluid -->
-    </nav>
-<div class="exibe_processo">
+       </div>
+     </div>
+   </div>
+   <footer>
+     
+   </footer>
+     <!-- jQuery -->
+    <script src="../admin/js/jquery.js"></script>
 
-</div>
-    <header>
-        <div class="header-content">
-            <div class="header-content-inner">
-              <?php
-                  $objProcesso = new Processo();
-                  $complemento = "ORDER BY id DESC";
-                  $lista = $objProcesso->listar($complemento);
-                  $linhas = "";
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../admin/js/bootstrap.min.js"></script>
 
-                  if ($lista != null) {
-                      foreach ($lista as $processo) {
-                        $exibeProcesso = $linhas."<strong><a href='cartao_ponto.php?id=".$processo->id."'>".$processo->nome_reclamante." Contra ".$processo->nome_reclamado.", ".$processo->num_processo."</strong><br />";
-                        echo $exibeProcesso ;
-                      }
-                  }
-               ?>
-            </div>
-        </div>
-    </header>
+
 
 </body>
 </html>
